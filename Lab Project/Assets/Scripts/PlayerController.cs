4d82d3a83056f52960da492bbd6d821464a46d71
cjbytheocean <cjrodenbush2024@gmail.com>
2026-01-20 18:49:36 -0500

    imported assets lol

---
+++ Lab Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float sensitivity;
+    [SerializeField] float sprintSpeed;
+
+    private float moveFB;
+    private float moveLR;
+    private float rotX;
+    private float rotY;
+
+    private Camera playerCam;
+    private CharacterController cc;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        cc = GetComponent<CharacterController>();
+        playerCam = transform.GetChild(0).GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float movementSpeed = speed;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movementSpeed = sprintSpeed;
+        }
+        else
+        {
+            movementSpeed = speed;
+        }
+
+        moveFB = Input.GetAxis("Vertical") * movementSpeed;
+        moveLR = Input.GetAxis("Horizontal") * movementSpeed;
+        rotX = Input.GetAxis("Mouse X") * sensitivity;
+        rotY = Input.GetAxis("Mouse Y") * sensitivity;
+
+        rotY -= Mathf.Clamp(rotY, -60f, 60f);
+
+        Vector3 movement = new Vector3(moveLR, 0, moveFB).normalized * movementSpeed;
+        transform.Rotate(0, rotX, 0);
+
+        playerCam.transform.localRotation = Quaternion.Euler(rotY, 0, 0);
+        movement = transform.rotation * movement;
+
+        cc.Move(movement * Time.deltaTime);
+        
+    }
+}
